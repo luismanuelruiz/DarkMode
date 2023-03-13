ea2a4867f902bf51b62350e01bda546727c02269
luismanuelruiz <112290690+luismanuelruiz@users.noreply.github.com>
2023-03-13 10:32:01 -0400

    2 files modified, 1 deleted

--- src/App.js
+++ src/App.js
@@ -4,22 +4,12 @@ import { useState, useEffect } from 'react';
 
 export default function App() {
 
-  const [data,setData]=useState(null);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((res) => res.json())
-    .then((data) => setData(data));
-  },[]);  
 
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      {data &&
-      data.map((item) =>{
-        return <p key={item.id}>{item.title}</p>
-      })}
     </div>
   );
 }
